6ebfe6288accad88096a9193b5e03d5a4e49aeb0
Eric R <rasliche@gmail.com>
2019-12-22 17:34:49 -0500

    commented out service worker initiation, but it's available

--- src/site/js/main.js
+++ src/site/js/main.js
@@ -1,3 +1,16 @@
 (function() {
   console.log('🔥🔥🔥 Static site template 💻 🍰');
+
+    // // ServiceWorker is a progressive technology. Ignore unsupported browswers.
+    // if ('serviceWorker' in navigator) {
+    //     console.log('CLIENT: ServiceWorker registration in progress.');
+    //     navigator.serviceWorker.register('/service-worker.js').then(function() {
+    //         console.log('CLIENT: ServiceWorker registration complete.');
+    //     }, function() {
+    //         console.log('CLIENT: ServiceWorker registration failed.');
+    //     });
+    // } else {
+    //     console.log('CLIENT: ServiceWorker is not supported.');
+    // }
+
 })();
