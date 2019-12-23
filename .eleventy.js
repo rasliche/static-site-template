1a0f0ba79fbede56e2f07db835391bbbba8b1b26
Eric R <rasliche@gmail.com>
2019-12-23 12:44:39 -0500

    Fixed the javascript passthrough directory

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addPassthroughCopy("src/js");
+  // eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/site/js");
 
   return {
     dir: {
