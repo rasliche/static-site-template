59df6ef448689fc3ac49255d5c12bfeb198782d7
Eric Raslich <rasliche@gmail.com>
2020-11-09 18:09:20 -0500

    Example environment variable for an 11ty site

---
+++ src/site/_data/environment.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // call environment.stripePublishableKey() in any of your
+    // templates to access this variable. Example:
+    // const stripe = Stripe('{{ environment.stripePublishableKey() }}')
+    //
+    // stripePublishableKey: () => {
+    //     return process.env.STRIPE_PUBLISHABLE_KEY
+    // }
+}
